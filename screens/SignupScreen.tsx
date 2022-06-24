ad015a95d399bd5b5d0619a209cb0c6748ec4a65
Hugo Garnier <hugo.garnier@ik.me>
2022-06-24 07:42:21 +0200

    fix 2

--- screens/SignupScreen.tsx
+++ screens/SignupScreen.tsx
@@ -1,17 +1,17 @@
-import React, { FC, useState } from 'react';
+import React, {FC, useState} from 'react';
 
 import Register from '@assets/svg/Register';
-import { RootStackScreenProps } from '@types';
-import { Button, Column, KText, KTextInput, Layout, Row } from '@ui-kit';
+import {RootStackScreenProps} from '@types';
+import {Button, Column, KText, KTextInput, Layout, Row} from '@ui-kit';
 
-import { signupDefault } from '../api/firebaseAuth';
+import {signupDefault} from '../api/firebaseAuth';
 
 export type SignupScreenProp = RootStackScreenProps<'Signup'>;
 
-const SignupScreen: FC<SignupScreenProp> = ({ navigation }) => {
-  const [email, setEmail] = useState<string>('hugo3@test.com');
-  const [password, setPassword] = useState<string>('azerty');
-  const [username, setUsername] = useState<string>('hugo');
+const SignupScreen: FC<SignupScreenProp> = ({navigation}) => {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [username, setUsername] = useState<string>('');
   const [error, setError] = useState<string>('');
 
   const setFirebaseSignupDefault = async () => {
@@ -26,7 +26,7 @@ const SignupScreen: FC<SignupScreenProp> = ({ navigation }) => {
   return (
     <Layout>
       <Column spacingV={40}>
-        <Register height={200} />
+        <Register height={200}/>
       </Column>
       <Column spacingV={20}>
         <KText variant="h3">Information de connexion</KText>
@@ -63,7 +63,7 @@ const SignupScreen: FC<SignupScreenProp> = ({ navigation }) => {
       </Column>
       <Column spacingV={20}>
         <Row>
-          <KText variant="body2" style={{ marginRight: 20 }}>
+          <KText variant="body2" style={{marginRight: 20}}>
             Pas de compte ?
           </KText>
           <Button onPress={() => navigation.navigate('Login')} variant="tertiary" size="small">
